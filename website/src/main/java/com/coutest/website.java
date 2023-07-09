a96ea0d35d4d34aa1e33681ae7262a0ed7c7c60b
Yann <yan_noudou@yahoo.fr>
2023-07-09 14:48:15 +0200

    first-commit

---
+++ website/src/main/java/com/coutest/website.java
@@ -0,0 +1,13 @@
+package com.coutest;
+
+/**
+ * This class is optional and represents the configuration for the website module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/website</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class website {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
